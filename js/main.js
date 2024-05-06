142dfd0ba451fbe95122c1c10abf35947f3db659
MaxVoicesDeveloper <maksslesh@gmail.com>
2024-05-06 22:38:59 +0300

    Разработано модальне окно формы логина V1 V1 - Будут и другие более аптимизированные версси данного продукта, так как стили css я благополучно взял с формы регистрации

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
 document.querySelector('.reg').addEventListener('click', function(){
-    document.querySelector('.modal-window').classList.toggle('open');
+    document.querySelector('.rega').classList.toggle('open');
 });
 
 document.querySelector('.to-close').addEventListener('click', function(){
-    document.querySelector('.modal-window').classList.toggle('open');
+    document.querySelector('.rega').classList.toggle('open');
 });
 
 document.querySelector('.lets').addEventListener('click', function(){
-    document.querySelector('.modal-window').classList.toggle('open');
+    document.querySelector('.rega').classList.toggle('open');
 });
+
+document.querySelector('.log').addEventListener('click', function(){
+    document.querySelector('.loga').classList.toggle('open');
+});
+
+document.querySelector('.closelog').addEventListener('click', function(){
+    document.querySelector('.loga').classList.toggle('open');
+});
+
+
