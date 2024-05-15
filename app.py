d90c1770b5cb1766d5d9485e1d48ba67e52a1773
QWerProg <ds.qwerprog04@mail.ru>
2024-05-15 20:39:26 +0300

    Изменения скрипта

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, url_for, redirect, flash, jsonify
+from flask import Flask, render_template, request, session, url_for, redirect, flash
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -6,9 +6,9 @@ app.secret_key = 'gggforforgg' # Замените 'your_secret_key' на ваш 
 
 # Настройка подключения к MySQL
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Maxonbd15)'
-app.config['MYSQL_DB'] = 'PlanB'
+app.config['MYSQL_USER'] = 'app_user'
+app.config['MYSQL_PASSWORD'] = 'app_user'
+app.config['MYSQL_DB'] = 'PlanBApp'
 
 mysql = MySQL(app)
 
@@ -21,39 +21,49 @@ def home():
             login = request.form.get('login')
             password = request.form.get('password')
             email = request.form.get('email')
-            account_type = request.form.get('account_type')
-            cur = mysql.connection.cursor()
-            cur.execute("SELECT * FROM users WHERE login = %s OR email = %s", (login, email))
-            if cur.fetchone():
-                return jsonify(success=False, message='Логин или почта уже существуют')
-            cur.execute("INSERT INTO users (name, login, password, email, account_type) VALUES (%s, %s, %s, %s, %s)", (name, login, password, email, account_type))
-            mysql.connection.commit()
-            flash('Registration successful.')
-            session['loggedin'] = True
-            session['id'] = cur.lastrowid
-            session['login'] = login
-            session['account_type'] = account_type  # Сохраняем тип аккаунта в сессии
-            return jsonify(success=True, message='Регистрация успешна', redirect_url=url_for('personal_account'))
+            if not name or not login or not password or not email:
+                flash('Please fill out all fields.')
+            else:
+                cur = mysql.connection.cursor()
+                cur.execute("SELECT * FROM t_users WHERE login = %s", [login])
+                user = cur.fetchone()
+                if user:
+                    flash('Registration successful.')
+                    session['loggedin'] = True
+                    session['id'] = cur.lastrowid
+                    session['login'] = login
+                    return redirect(url_for('personal_account')) # Перенаправление на personal_account
+                else:
+                    # Маппинг ФИО (можно без отчества)
+                    last_name = ''
+                    first_name = ''
+                    try:
+                        last_name, first_name, second_name = name.split(' ')
+                    except ValueError:
+                        last_name, first_name = name.split(' ')
+                        second_name = ''
+
+                    cur.execute("INSERT INTO t_users (login, password, email, last_name, first_name, second_name) VALUES (%s, %s, %s, %s, %s, %s)", 
+                                (login, password, email, last_name, first_name, second_name))
+                    mysql.connection.commit()
+                    flash('Registration successful.')
+                    session['loggedin'] = True
+                    session['id'] = cur.lastrowid
+                    session['login'] = login
+                    return redirect(url_for('personal_account')) # Перенаправление на personal_account
         elif action == 'login':
             login = request.form.get('login')
             password = request.form.get('password')
             cur = mysql.connection.cursor()
-            cur.execute("SELECT * FROM users WHERE login = %s AND password = %s", (login, password))
+            cur.execute("SELECT * FROM t_users WHERE login = %s AND password = %s", (login, password))
             user = cur.fetchone()
             if user:
                 session['loggedin'] = True
                 session['id'] = user[0]
                 session['login'] = user[1]
-                session['account_type'] = user[4]  # Загружаем тип аккаунта из базы данных
                 flash('Login successful.')
-                return jsonify(success=True, message='Вход в систему успешен', redirect_url=url_for('personal_account'))
-            else:
-                flash('Login unsuccessful.')
-                return jsonify(success=False, message='Неверный логин или пароль')
-        else:
-            return render_template('index.html')
-    else:
-        return render_template('index.html')
+                return redirect(url_for('personal_account')) # Перенаправление на personal_account
+    return render_template('index.html')
 
 def translate_account_type(account_type):
     if account_type == 'employer':
@@ -67,13 +77,14 @@ def translate_account_type(account_type):
 def personal_account():
     if 'loggedin' in session:
         cur = mysql.connection.cursor()
-        cur.execute("SELECT account_type FROM users WHERE login = %s", (session['login'],))
+        cur.execute("SELECT account_type FROM t_users WHERE login = %s", (session['login'],))
         account_type = cur.fetchone()[0]
         session['account_type'] = account_type  # Сохраняем тип аккаунта в сессии
         return render_template('personalaccount.html', translate_account_type=translate_account_type)  # Передаем функцию в шаблон
     else:
         flash('You must be logged in to view that page.')
         return redirect(url_for('home'))
+
 
 @app.route('/logout')
 def logout():
@@ -83,8 +94,6 @@ def logout():
     session.pop('login', None)
     # Перенаправление на главную страницу
     return redirect(url_for('home'))
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
